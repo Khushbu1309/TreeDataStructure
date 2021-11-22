044da218c93fd9956ea39ddbec7e505f43aae8a4
Khushbu1309 <74493334+Khushbu1309@users.noreply.github.com>
2021-11-23 00:42:25 +0530

    BinaryTreeNodeClass

---
+++ binaryTreeNode.java
@@ -0,0 +1,15 @@
+package TreeDataStructure;
+
+@SuppressWarnings("hiding")
+public class binaryTreeNode<Integer>{
+	public int data;
+	public binaryTreeNode<Integer>rightChild;
+	public binaryTreeNode<Integer>leftChild;
+
+	public binaryTreeNode( int data) {
+		this.data=data;
+		
+	
+	}
+
+}
